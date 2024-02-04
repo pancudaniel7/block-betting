f74a3901c6dea030c1be6bef286c275ce9b30bf5
Daniel <pancudaniel7@users.noreply.github.com>
2024-02-04 21:55:04 +0200

    Modified api naming

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const configureRoutes = (app) => {
   app.use('/api/auth', require('./api/auth'));
   app.use('/api/users', require('./api/users'));
   app.use('/api/chips', require('./api/chips'));
-  app.use('/api/contracts/storage', require('./api/contracts/simpleStorage'));
+  app.use('/api/contracts/storage', require('./api/contracts/danielapitest'));
   
   app.use('/', (req, res) => {
     res.status(200).send('Welcome to  Poker!');
